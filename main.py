4733cbf58dc8f091a6d7e3d05728e23105c07d0d
noraahlman23 <819446@mystma.org>
2021-02-25 16:28:08 +0000

    calendar

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def calendar(c):
         if (c) == months[i]:
             return i + 1
 
-    return p + "is not a planet"
+    return p + "is not a month"
 
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 print (calendar("January"))
