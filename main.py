75a97170d1388db5158b175efa17e69e33bcc4b4
noraahlman23 <819446@mystma.org>
2021-02-25 16:21:23 +0000

    calandar

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def calandar(c):
+    for i in range (0, len(months)):
+        if (c) == months[i]:
+            return i + 1
+
+    return p + "is not a planet"
+
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+print (calandar("January"))
